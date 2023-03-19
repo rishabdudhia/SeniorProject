3d55b2cee38e8411f3726c2f409ef121ef48a7e1
shruti <shruti.s.jawal@gmail.com>
2023-03-18 22:41:05 -0700

    Add modified logfile functions

--- backend/logfile_functions.py
+++ backend/logfile_functions.py
@@ -1,17 +1,17 @@
 import math
-from flask import Flask
+import datetime
+from enum import Enum
 
 '''
-Function to create LogFile (initialize logFile here)
-Function to write to LogFile
+Function to create logfile and logfile_copy
+Function to write to logfile by user
+Function to write to logfile by us each time user hits enter
 
-This file is the only way to access the logfile and is general enough to used anytime 
-
-ADD: every line needs time and type (e.g. INFO, ERROR)
-    two functions: one for user comment, another for us
-        need access to username so I can put it in the user comment.
-
+This file is the only way to access either logfile.
 '''
+
+tab = '  '
+logMessage = Enum('logMessage', ['FATAL', 'ERROR', 'WARN', 'INFO', 'DEBUG', 'TRACE'])
 
 def createLogFile():
     file1 = open("logfile.txt", 'w')        #overwrites the entire file
@@ -20,13 +20,29 @@ def createLogFile():
     file2.close()
     return
 
-def addCommenttoLogFile(comment):
+def addUserCommenttoLogFile(comment):
+    time = datetime.datetime.now()
+    entireMessage = str(time) + tab + logMessage.INFO.name + tab + "Employee wants to make a note that \"" + comment + "\"\n"
+
     file1 = open("logfile.txt", 'a')        #appends to the file if it does not exist
-    file1.write(comment)
+    file1.write(entireMessage)
     file1.close()
 
     file2 = open("logfile_copy.txt", 'a')   #appends to the file if it does not exist
-    file2.write(comment)
+    file2.write(entireMessage)
+    file2.close()
+    return
+
+def addContainerCommenttoLogFile(comment):
+    time = datetime.datetime.now()
+    entireMessage = str(time) + tab + logMessage.INFO.name + tab + comment + '\n'
+
+    file1 = open("logfile.txt", 'a')        #appends to the file if it does not exist
+    file1.write(entireMessage)
+    file1.close()
+
+    file2 = open("logfile_copy.txt", 'a')   #appends to the file if it does not exist
+    file2.write(entireMessage)
     file2.close()
     return
 
@@ -36,11 +52,14 @@ def printLogFile():
     return
 
 
+"""
 def main():
     createLogFile()
-    addCommenttoLogFile("hello")
+    addUserCommenttoLogFile("I noticed that “Apple Valley Machine parts” is about 10% over its stated weight, may be due to the rain on top, no further action taken.")
+    addUserCommenttoLogFile("I notice that the “Walmart Toasters Moreno Valley South” container has a large fresh-looking dent on the door. However, door was not breeched, so just sent a photo to head office and I am continuing with the cycle.")
     printLogFile()
     return
 
 if __name__ == "__main__":
     main()
+"""
