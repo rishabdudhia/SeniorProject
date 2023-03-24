3b2897f5486d0038b1b81820915df395e0fe8e48
shruti <shruti.s.jawal@gmail.com>
2023-03-23 19:12:08 -0700

    add login functions

--- flask_shash/container.py
+++ flask_shash/container.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from werkzeug.utils import secure_filename
+import datetime
+from enum import Enum
 
 class Back:
     unloadList = []
@@ -121,6 +123,53 @@ def solveBalance():
     return("", 200)
 
 
+
+
+'''
+Function to create logfile and logfile_copy
+Function to write to logfile by user
+Function to write to logfile by us each time user hits enter
+'''
+tab = '  '
+logMessage = Enum('logMessage', ['FATAL', 'ERROR', 'WARN', 'INFO', 'DEBUG', 'TRACE'])
+
+def createLogFile():
+    file1 = open("logfile.txt", 'w')        #overwrites the entire file
+    file1.close()
+    file2 = open("logfile_copy.txt", 'w')   #overwrites the entire file
+    file2.close()
+    return
+
+def addUserCommenttoLogFile(comment):
+    time = datetime.datetime.now()
+    entireMessage = str(time) + tab + logMessage.INFO.name + tab + "Employee wants to make a note that \"" + comment + "\"\n"
+
+    file1 = open("logfile.txt", 'a')        #appends to the file if it does not exist
+    file1.write(entireMessage)
+    file1.close()
+
+    file2 = open("logfile_copy.txt", 'a')   #appends to the file if it does not exist
+    file2.write(entireMessage)
+    file2.close()
+    return
+
+def addContainerCommenttoLogFile(comment):
+    time = datetime.datetime.now()
+    entireMessage = str(time) + tab + logMessage.INFO.name + tab + comment + '\n'
+
+    file1 = open("logfile.txt", 'a')        #appends to the file if it does not exist
+    file1.write(entireMessage)
+    file1.close()
+
+    file2 = open("logfile_copy.txt", 'a')   #appends to the file if it does not exist
+    file2.write(entireMessage)
+    file2.close()
+    return
+
+def printLogFile():
+    file1 = open("logfile.txt", 'r')        #opens to read the file
+    print(file1.read())
+    return
 
 
 #Allows site to be hosted by running python script
